b3defa94488191e4ea5ecdfbee788c7c6423969b
Rafin <rafale25_tavares@hotmail.ccmom>
2021-10-15 21:43:24 -0300

    master

--- Controller.js
+++ Controller.js
@@ -80,6 +80,29 @@ app.post('/itempedidos', async(req, res)=>{
     });
 });
 
+
+
+app.get('/ofertaservicos', async(req, res)=>{
+    await servico.count('id').then(function(servicos){
+        res.json({servicos});
+    });
+});
+
+app.get('/servico/:id', async(req, res)=>{
+    await servico.findByPk(req.params.id)
+    .then((serv) =>{
+        return res.json({
+            error: false,
+            serv
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro: Não foi possível conectar-se!"
+        });
+    });
+});
+
 app.get('/listaservicos', async(req, res)=>{
     await servico.findAll({
         raw: true
@@ -88,8 +111,46 @@ app.get('/listaservicos', async(req, res)=>{
     });
 });
 
+app.get('/listaclientes', async(req, res)=>{
+    await cliente.findAll({
+        raw: true
+    }).then(function(clientes){
+        res.json({clientes})
+    });
+});
+
+app.get('/listaclientesasc', async(req, res)=>{
+    await cliente.findAll({
+        order: [['nascimento', 'ASC']]
+    }).then(function(clientes){
+        res.json({clientes})
+    });
+});
+
+app.put('/atualizaservico', async(req, res)=>{
+    await servico.update(req.body,{
+        where: {id: req.body.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Serviço foi altrerado com sucesso!"
+        })
+    }).catch(function(erro){
+        return res.status(400).json({
+            erro: true,
+            message: "Erro na alteração do serviço"
+        })
+    })
+});
+
+app.get('/pedidos/:id', async(req, res)=>{
+    await pedido.findByPk(req.params.id,{include:[{all:true}]})
+    .then(ped=>{
+        return res.json({ped})
+    })
+})
 let port = process.env.PORT || 3001;
 
 app.listen(port,(req,res)=>{
     console.log('Servidor ativo: http://localhost:3001');
-})
+});
